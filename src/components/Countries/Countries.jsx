047a60055837aa8513a10f35d73aa4d9d1442044
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-10 19:19:42 +0530

    Added a visited country list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,17 +5,36 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=>setCountries(data))
     },[]);
+    const handleVistedCountry = country =>{
+        console.log("Add this to your visited country");
+        // console.log(country);
+        //copy old info and make a new array with a new info
+        const newVisitedCountries = [...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries);
+    }
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h3>Visited countries: {visitedCountries.length}</h3>
+                <ul>
+                    {
+                        visitedCountries.map(country=><li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
             {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                //we will send function as props
+                countries.map(country => <Country key={country.cca3}
+                handleVistedCountry={handleVistedCountry}
+                    country={country}></Country>)
             }</div>
         </div>
     );
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country,handleVistedCountry}) => {
     const {name, flags, population, area, cca3} = country;
     //default value is false means not visited
     const [visited,setVisited] = useState(false);
@@ -8,15 +8,18 @@ const Country = ({country}) => {
         //for toggle
         setVisited(!visited);
     }
+    // console.log(handleVistedCountry);
+
     return (
         //conditional styling
         <div className={`country ${visited && 'visited'}`}>
-            <h3>Name : {name.common}</h3>
+            <h3 style={{color: visited ? 'black':'white'}}>Name : {name.common}</h3>
             <img className='flag-img' src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
-            <button onClick={handleVisited}>{visited ? 'Visited':'Going to Visit'}</button>
+            <button style={{marginRight:'5px'}} onClick={()=>handleVistedCountry(country)}>Mark Visited</button>
+            <button onClick={handleVisited}>{visited ? 'Visited':'Going to Visit'}</button><br />
             {visited ? 'I have visited this country!':'I want to visit this country!'}
         </div>
     );
