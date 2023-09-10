f97a8d39e99ba63c713f6e196cf93f006c4b22cf
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-10 18:40:24 +0530

    Changed background color if a country is marked as visited

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -9,7 +9,8 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
     return (
-        <div className="country">
+        //conditional styling
+        <div className={`country ${visited && 'visited'}`}>
             <h3>Name : {name.common}</h3>
             <img className='flag-img' src={flags.png} alt="" />
             <p>Population: {population}</p>
