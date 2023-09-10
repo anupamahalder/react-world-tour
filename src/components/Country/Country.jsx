0613e0589d46ea3bab6181fc1cf72b3f1611f5cd
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-10 18:36:36 +0530

    Added visited and going to visit button where we can toggle each time when button is clicked!

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, population, area, cca3} = country;
+    //default value is false means not visited
+    const [visited,setVisited] = useState(false);
+    const handleVisited = ()=>{
+        //for toggle
+        setVisited(!visited);
+    }
     return (
         <div className="country">
             <h3>Name : {name.common}</h3>
-            <img src={flags.png} alt="" />
+            <img className='flag-img' src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited':'Going to Visit'}</button>
+            {visited ? 'I have visited this country!':'I want to visit this country!'}
         </div>
     );
 };
